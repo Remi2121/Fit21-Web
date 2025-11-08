fbf7e6645c4d68efd9c65350d3b197b204bc8910
Remi <remiluke12@gmail.com>
2025-11-08 17:41:00 +0530

    quiz added

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,11 +4,13 @@ import Owners from "./owners/owners.jsx";
 import Testimonials from "./Testimonials/Testimonials.jsx";
 import Fooder from "./components/fooder/Fooder.jsx";
 import ContactUs from "./contactus/contactus..jsx";
+import Quiz from "./quiz/quiz.jsx";
 
 export default function Home() {
   return (
     <>
       <Fit />
+      <Quiz/>
       <Owners />
       <Testimonials />
       <ContactUs />
