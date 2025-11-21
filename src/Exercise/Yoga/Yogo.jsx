d57d2112f4eaee45c05bf1f386f5d78d0feeb199
Remi <remiluke12@gmail.com>
2025-11-21 22:25:42 +0530

    attdence marking enabled,rules enable in admin suite,Bortoe fixed

--- src/Exercise/Yoga/Yogo.jsx
+++ src/Exercise/Yoga/Yogo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BigToe from "./Big Toe/Big-Toe"
 import Bridge from "./Bridge/BridgePose"
+import WarriorIII from "./WarriorIII/WarriorIII"
 const Yogo = () => {
   return (
     <div>
