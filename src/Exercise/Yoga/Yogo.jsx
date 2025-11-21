15945d72ace77ca12c294cc9957438154fd3f4fc
Dhanoshigan Ratnaraj <dhanoshiganratnarajah2001@gmail.com>
2025-11-20 20:59:49 -0800

    warrior3.jsx is updated

--- src/Exercise/Yoga/Yogo.jsx
+++ src/Exercise/Yoga/Yogo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import BigToe from "./Big Toe/Big-Toe"
 import Bridge from "./Bridge/BridgePose"
+import WarriorIII from "./WarriorIII/WarriorIII"
 const Yogo = () => {
   return (
     <div>
       <BigToe/>
       <Bridge/>
+      <WarriorIII/>
     </div>
   )
 }
